cd02ed6417138a3009a37200340d19058e146139
juansgaitan <juans.gaitan@gmail.com>
2020-03-17 12:36:03 +0100

    Change arp for nmap

--- main.py
+++ main.py
@@ -4,62 +4,88 @@ from datetime import datetime
 from os import system, name
 from atexit import register
 from time import sleep
+from src import ip
 from re import sub
+
+IPS_DIR = "ips"
+
+# TODO:
+# add curses
+# from curses import initscr, endwin
+# example:
+#    def users(self, instances):
+#        self._screen = initscr()
+#        register(endwin)
+#        # ...
+
+#    def _handle_output(self, output):
+#        self._screen.clear()
+#        for i, line in enumerate(output.splitlines()):
+#            self._screen.addstr(i, 2, line.strip())
+#        self._screen.refresh()
+#        # ...
 
 
 class MacGraph:
-    GRAPH_PATH = "./graph.json"
-
     def __init__(self):
+        self.info = ip.get_ip_info()
+        self.ip = self.info.get("ip")
+        self.inet = ip.get_inet()
+
+        self.graph_path = f"./{IPS_DIR}/{self.ip}.json"
         self.graph = self._load_graph()
         register(self._save_graph)
 
     def _load_graph(self):
         try:
-            with open(self.GRAPH_PATH, "r") as graph:
+            with open(self.graph_path, "r") as graph:
                 print("Previous graph LOADED.")
                 return json_load(graph)
         except Exception as exc:
             print("Previous graph NOT LOADED.")
             print(exc)
-            return {}
+            return {"info": self.info}
 
     def _save_graph(self):
-        with open(self.GRAPH_PATH, "w") as file:
+        with open(self.graph_path, "w") as file:
             print("Saving graph...")
             json_dump(self.graph, file)
             print("Graph saved.")
 
     def _update_connections(self, stdout):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         connected, disconnected, new = 0, 0, 0
         previous_macs = {*self.graph.keys()}
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         print("New connected MACs:")
         for line in stdout.splitlines():
-            ip, rest = line.split("at")
-            mac, more = rest.split("on")
-            ip = sub(r"[^0-9\.]", "", ip)
-            mac = mac.strip().upper()
+            if "MAC" not in line:
+                continue
+            _, rest = line.split(": ", 1)
+            mac, system = rest.split(" ", 1)
 
-            if "(" not in mac or ")" not in mac:
-                state = self.graph.get(mac, {})
-                if state:
-                    is_connected = state.get('connected')
-                    if is_connected == False:
-                        print("> Reconnected:", mac)
-                self.graph[mac] = {"last_seen": now, "ip": ip, "connected": True}
+            system = sub(r"\(|\)", "", system.strip())
+            mac = mac.strip()
 
-                if mac in previous_macs:
-                    previous_macs.remove(mac)
-                    connected += 1
-                else:
-                    print(">", mac)
-                    new += 1
+            state = self.graph.get(mac, {})
+            if state:
+                is_connected = state.get('connected')
+                if is_connected == False:
+                    print("> Reconnected:", mac)
+            self.graph[mac] = {"last_seen": now, "connected": True, "system": system}
+
+            if mac in previous_macs:
+                previous_macs.remove(mac)
+                connected += 1
+            else:
+                print(">", mac)
+                new += 1
         print()
 
         print("Recently disconnected MACs:")
         for previous_mac in previous_macs:
+            if previous_mac == "info":
+                continue
             prev_state = self.graph.get(previous_mac)
             if prev_state.get('connected') == True:
                 print(">", previous_mac)
@@ -69,9 +95,9 @@ class MacGraph:
 
         return connected, disconnected, new
 
-    def _arp(self):
+    def _nmap(self):
         kwargs = {'stdout': PIPE, 'stderr': PIPE, 'universal_newlines': True}
-        with Popen(['sudo', 'arp', '-a'], **kwargs) as proc:
+        with Popen(['sudo', 'nmap', '-sn', self.inet], **kwargs) as proc:
             return proc.communicate()
 
     def _print(self):
@@ -82,10 +108,10 @@ class MacGraph:
         while True:
             print("> Loading Address Resolution Protocol...")
             self._run()
-            sleep(1)
+            sleep(15)
 
     def _run(self):
-        stdout, stderr = self._arp()
+        stdout, stderr = self._nmap()
         if name == "posix":
             system('clear')
         else:
--- src/ip.py
+++ src/ip.py
@@ -1,14 +1,41 @@
 from urllib.request import Request, urlopen
+from subprocess import check_output
+from ipaddress import ip_address
+from time import sleep
 from json import loads
-import ipaddress
 
 
-def get_current_ip():
+def get_inet():
+    output = check_output(["ifconfig", "en0", "inet"])
+    for line in output.decode().splitlines():
+        if "inet" in line:
+            _, inet = line.strip().split()[:2]
+            if ip_address(inet).is_private:
+                vals = inet.split(".")[:-1] + ["*"]
+                return ".".join(vals)
+            else:
+                print(line)
+            return inet
+    return ""
+
+
+def get_ip_info(retry_times=0):
+    if retry_times == 0:
+        print("Getting IP info...")
+    else:
+        print("Retrying...")
+        sleep(3)
     req = Request(url="https://ifconfig.co/json", headers={'User-Agent': 'curl/7.54.0'})
-    return loads(urlopen(req).read().decode())
+    try:
+        res = urlopen(req).read()
+        return loads(res.decode())
+    except Exception as exc:
+        if retry_times < 5:
+            return get_ip_info(retry_times=retry_times+1)
+        return {}
 
 
 if __name__ == "__main__":
-    print("IP")
-    ip = get_current_ip()
-    print(type(ip), ip)
+    info = get_inet()
+    print(info)
+    print("Current IP:", info)
